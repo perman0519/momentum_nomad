ba9ef7f79900a7d039169672ca4e3600c1c02869
perman0519 <102403228+perman0519@users.noreply.github.com>
2022-08-14 07:01:16 +0900

    0814 ghpages

---
+++ weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "149c7fea417355a8019a29e7417ceb2e";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      const temp = Math.ceil(data.main.temp - 273.15);
+      weather.innerText = `${data.weather[0].main} / ${temp}'C`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
